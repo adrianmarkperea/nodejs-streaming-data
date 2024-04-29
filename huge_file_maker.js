e1b294d7265c622f7e94bbfe595eab46efafd04c
Adrian Perea <adrian@thinkingmachin.es>
2024-04-29 22:44:35 +0800

    feat: fixes filename output of huge_file_maker.js

--- huge_file_maker.js
+++ huge_file_maker.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 
-const file = fs.createWriteStream("./bigfile.txt")
+const file = fs.createWriteStream("./hugefile.txt")
 
 for (let i = 0; i <= 1e6; i++) {
   file.write("Lorem ipsum dolor sit amet")
